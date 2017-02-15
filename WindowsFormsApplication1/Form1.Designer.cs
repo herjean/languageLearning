2fabb948cf006a8afbcc8ae3a135ea3a6241181b
herjean <wong0786@e.ntu.edu.sg>
2017-02-15 22:53:09 +0800

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.thaiLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.phoneticLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // thaiLabel
+            // 
+            this.thaiLabel.AutoSize = true;
+            this.thaiLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thaiLabel.Location = new System.Drawing.Point(23, 155);
+            this.thaiLabel.Name = "thaiLabel";
+            this.thaiLabel.Size = new System.Drawing.Size(209, 76);
+            this.thaiLabel.TabIndex = 0;
+            this.thaiLabel.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(376, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // phoneticLabel
+            // 
+            this.phoneticLabel.AutoSize = true;
+            this.phoneticLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneticLabel.Location = new System.Drawing.Point(263, 155);
+            this.phoneticLabel.Name = "phoneticLabel";
+            this.phoneticLabel.Size = new System.Drawing.Size(209, 76);
+            this.phoneticLabel.TabIndex = 3;
+            this.phoneticLabel.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 524);
+            this.Controls.Add(this.phoneticLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.thaiLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label thaiLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label phoneticLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        //DECLARATION OF VARIABLES
+        int wordCount = 0;
+        List<string> thaiWords = new List<string>();
+        List<string> phoneticWords = new List<string>();
+        Timer countDownTimer = new Timer();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region DISPLAY THAI WORDS
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConnectToAccessDB();
+
+            //GET TOTAL NUMBER OF WORDS
+            wordCount = thaiWords.Count();
+
+            //INITIALISE TIMER
+            countDownTimer.Interval = 5000;
+            countDownTimer.Enabled = true;
+            countDownTimer.Tick += new EventHandler(countDownTimer_Tick);
+        }
+
+        private async void countDownTimer_Tick(object sender, EventArgs e)
+        {
+            //RANDOMISE DISPLAY OF EVERY WORD WITH 5 SECONDS INTERVAL
+            Random r = new Random();
+            int rInt = r.Next(0, wordCount);
+
+            phoneticLabel.Text = "";
+            thaiLabel.Text = thaiWords[rInt].ToString();
+
+            //DISPLAY PHONETIC AFTER 2 SECONDS
+            await DisplayPhoneticWords(rInt);
+        }
+
+        private async Task DisplayPhoneticWords(int rInt)
+        {
+            await Task.Delay(2000);
+            phoneticLabel.Text = phoneticWords[rInt].ToString();
+        }
+
+        #endregion
+
+        private void ConnectToAccessDB()
+        {
+
+            string dbConnString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+
+            //CONNECT TO ACCESS DATABASE
+            using (OleDbConnection conn = new OleDbConnection(dbConnString))
+            using (OleDbCommand cmd = new OleDbCommand("SELECT * from Thai_Words", conn))
+            {
+                conn.Open();
+                OleDbDataReader reader = cmd.ExecuteReader();
+
+                //GET LIST OF WORDS
+                while (reader.Read())
+                {
+                    thaiWords.Add(reader["thai"].ToString());
+                    phoneticWords.Add(reader["english"].ToString());
+                }
+                conn.Close();
+            }
+        }
+
+    }
+}
